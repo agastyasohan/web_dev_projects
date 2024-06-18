eb5dba0ee165187e0d08d340ebe29d7a0e92132e
agastyasohan <sohandp123@gmail.com>
2024-06-18 22:21:14 +0530

    styling the page (HTML/CSS and Bootstrap)

---
+++ etch-a-sketch/app.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.board');
+
+// for(let i=1;i<=256;i++){
+//     const div = document.createElement("div");
+//     div.setAttribute('id',i);
+//     div.innerText = i;
+//     container.append(div);
+// };
+
+
