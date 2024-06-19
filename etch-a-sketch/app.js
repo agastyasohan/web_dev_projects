06ecd9a200c50eca3f51e9e98952b39c785bde90
agastyasohan <sohandp123@gmail.com>
2024-06-19 10:01:05 +0530

    createBoard function created

--- etch-a-sketch/app.js
+++ etch-a-sketch/app.js
@@ -1,4 +1,3 @@
-const container = document.querySelector('.board');
 
 // for(let i=1;i<=256;i++){
 //     const div = document.createElement("div");
@@ -8,3 +7,25 @@ const container = document.querySelector('.board');
 // };
 
 
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+    createBoard(32);
+  });
+
+const createBoard = (size) => {
+    const board = document.querySelector('.board');
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+
+    let numDiv = size*size;
+
+    for(let i=0;i<numDiv;i++){
+      const div = document.createElement('div')
+      div.style.backgroundColor = "yellow"
+      board.insertAdjacentElement('beforeend', div)
+    }
+      
+  
+    
+}
