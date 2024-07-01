44aaeb6054abf0df91713861e9c7124f7f0fd985
agastyasohan <sohandp123@gmail.com>
2024-07-02 00:32:58 +0530

    added a function addBookToLibrary() that take user's input and store the new Book objects into an array myLibrary[].

--- library_app/app.js
+++ library_app/app.js
@@ -1,43 +1,53 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const newBookBtn = document.querySelector('#newBookBtn');
     const add_book = document.querySelector('#addBookDialog');
     const submitBtn = document.querySelector('#submitBtn');
-    const addBookForm = document.querySelector('addBookForm');
-    const bookCards = document.querySelector('.bookCards');
+    const addBookForm = document.querySelector('#addBookForm');
 
     newBookBtn.addEventListener('click', (e) => {
         add_book.showModal();
     });
 
-    submitBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        const title = document.querySelector('#title').value;
-        const author = document.querySelector('#author').vlaue;
-        const pages = document.querySelector('#pages').value;
-        const read = document.querySelector('#read').checked? "Finished" : "Not Finished";
-
-        const bookCard = document.createElement('div');
-
-        bookCard.classList.add('bookCard');
-
-        bookCard.innerHTML = `
-        <h3> ${title} </h3>
-        <div> </div>
-        <p> by ${author} </p>
-        <p> ${pages} Pages </p>
-        <p> ${read} </p>
-        <button> Change status </button>
-        <button> Remove </button>
-        `
-
-        bookCards.appendChild(bookCard);
+    submitBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        addBookToLibrary();
+        displayBooks();
 
         add_book.close();
         addBookForm.reset();
     });
 
-})
+    const myLibrary = [];
 
+    function Book(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
+    //default books to add
+    let book1 = new Book("The Alchemist", "Paulo Coelho", 156, "Finished");
+    let book2 = new Book("Mastery", "Robert Greene", 344, "Finsihed" );
+
+    myLibrary.push(book1);
+    myLibrary.push(book2);
+
+    function addBookToLibrary() {
+        let titleInput = document.querySelector('#title').value;
+        let authorInput = document.querySelector('#author').value; 
+        let pagesInput = document.querySelector('#pages').value;
+        let readInput = document.querySelector('#read').checked ? "Finished" : "Not Finished";
+        let newBook = new Book(titleInput, authorInput, pagesInput, readInput);
+        myLibrary.push(newBook);
+        console.log(myLibrary);
+    }
+
+    function displayBooks () {
+        const cardsContainer = document.querySelector('.bookCards');
+
+        // Clear library
+        cardsContainer.innerHTML = '';
+        
+    }
+});
